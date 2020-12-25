a7c3df77370c2171724ec7dd6d0bb54e511d2597
Nevcan Uludas <nevcan@uludas.com>
2020-12-25 21:49:54 +0300

    Fix, typos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tnyFaceDedector.loadFromUri('/models'),
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
@@ -18,7 +18,7 @@ function startVideo () {
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
-  const dislaySize = { width: video.childElementCountwidth, height: video.height }
+  const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
